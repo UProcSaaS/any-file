a84ca9d153d7ae4b1af95a09588ff5a69e3b7751
Miquel Colomer <mcolomer@gmail.com>
2016-08-23 18:00:51 +0200

    + fixed errors

--- test/test.js
+++ test/test.js
@@ -21,9 +21,6 @@ describe('anyfile', function () {
 
   it('ftp copy to local', function (done) {
     anyfile.from("ftp://anonymous:miemail%40gmail.com@speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
-      if (fs.exists('100KB.zip')) {
-        fs.unlinkSync('100KB.zip');
-      }
       assert.ok(res);
       done();
     });
@@ -31,9 +28,6 @@ describe('anyfile', function () {
 
   it('http copy to local', function (done) {
     anyfile.from("http://speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
-      if (fs.exists('100KB.zip')) {
-        fs.unlinkSync('100KB.zip');
-      }
       assert.ok(res);
       done();
     });
@@ -41,9 +35,6 @@ describe('anyfile', function () {
 
   it('http auth copy to local', function (done) {
     anyfile.from("http://anonymous:miemail%40gmail.com@speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
-      if (fs.exists('100KB.zip')) {
-        fs.unlinkSync('100KB.zip');
-      }
       assert.ok(res);
       done();
     });
@@ -51,9 +42,6 @@ describe('anyfile', function () {
 
   it('sftp copy to local', function (done) {
     anyfile.from("scp://demo:password@test.rebex.net:/readme.txt").to("readme.txt", function(err, res) {
-      if (fs.exists('readme.txt')) {
-        fs.unlinkSync('readme.txt');
-      }
       assert.ok(res);
       done();
     });
@@ -61,9 +49,6 @@ describe('anyfile', function () {
 
   it('sftp copy to s3', function (done) {
     anyfile.from("scp://demo:password@test.rebex.net/readme.txt").to("s3://AKIAIZHM3T2QFIRSVQ5A:gxxxYv+PuyihUrg0EqJ8U1C0pxBwxZGPO0U2DuhX@s3.amazon.com/any-file-us/readme.txt", function(err, res) {
-      if (fs.exists('readme.txt')) {
-        fs.unlinkSync('readme.txt');
-      }
       assert.ok(res);
       done();
     });
@@ -71,9 +56,6 @@ describe('anyfile', function () {
 
   it('s3 copy to s3', function (done) {
     anyfile.from("s3://AKIAIZHM3T2QFIRSVQ5A:gxxxYv+PuyihUrg0EqJ8U1C0pxBwxZGPO0U2DuhX@s3.amazon.com/any-file-us/readme.txt").to("s3://AKIAIZHM3T2QFIRSVQ5A:gxxxYv+PuyihUrg0EqJ8U1C0pxBwxZGPO0U2DuhX@s3.amazon.com/any-file-us/readme-final.txt", function(err, res) {
-      if (fs.exists('readme.txt')) {
-        fs.unlinkSync('readme.txt');
-      }
       assert.ok(res);
       done();
     });
