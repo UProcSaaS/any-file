d0b2258cf356610372803f0a57fef9f5ce8e1273
Miquel Colomer <mcolomer@gmail.com>
2016-10-15 12:25:44 +0200

    + added delete test files

--- test/test.js
+++ test/test.js
@@ -5,6 +5,11 @@ var fs = require('fs'),
     anyfile;
 
 describe('anyfile', function () {
+
+  var deleteFile = function(filePath, callback) {
+    fs.unlink(filePath, callback);
+  };
+
   this.timeout(10000);//10 seconds
   
   beforeEach(function(done){
@@ -19,34 +24,40 @@ describe('anyfile', function () {
   it('should have main methods', function () {
     assert.ok(anyfile.from);
   });
-
   
   it('ftp copy to local', function (done) {
     anyfile.from("ftp://anonymous:miemail%40gmail.com@speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
       debug(err);
       assert.ok(res);
-      done();
+      deleteFile("100KB.zip", done);
     });
   });
   
   it('http copy to local', function (done) {
     anyfile.from("http://speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
       assert.ok(res);
-      done();
+      deleteFile("100KB.zip", done);
+    });
+  });
+
+  it('https copy to local', function (done) {
+    anyfile.from("https://docs.uproc.io/pdf/resumen_del_servicio_ES.pdf").to("resumen_del_servicio_ES.pdf", function(err, res) {
+      assert.ok(res);
+      deleteFile("resumen_del_servicio_ES.pdf", done);
     });
   });
 
   it('http auth copy to local', function (done) {
     anyfile.from("http://anonymous:miemail%40gmail.com@speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
       assert.ok(res);
-      done();
+      deleteFile("100KB.zip", done);
     });
   });
 
   it('sftp copy to local', function (done) {
-    anyfile.from("scp://demo:password@test.rebex.net:/readme.txt").to("readme.txt", function(err, res) {
+    anyfile.from("scp://demo:password@test.rebex.net/readme.txt").to("readme.txt", function(err, res) {
       assert.ok(res);
-      done();
+      deleteFile("readme.txt", done);
     });
   });
 
@@ -67,14 +78,14 @@ describe('anyfile', function () {
   it('compress - s3 copy to local gz', function (done) {
     anyfile.from("s3://AKIAIZHM3T2QFIRSVQ5A:gxxxYv+PuyihUrg0EqJ8U1C0pxBwxZGPO0U2DuhX@s3.amazon.com/any-file-us/readme.txt").to("readme-final.txt.gz", function(err, res) {
       assert.ok(res);
-      done();
+      deleteFile("readme-final.txt.gz", done);
     });
   });
 
   it('compress - s3 copy to local zip', function (done) {
     anyfile.from("s3://AKIAIZHM3T2QFIRSVQ5A:gxxxYv+PuyihUrg0EqJ8U1C0pxBwxZGPO0U2DuhX@s3.amazon.com/any-file-us/readme.txt").to("readme-final.txt.zip", function(err, res) {
       assert.ok(res);
-      done();
+      deleteFile("readme-final.txt.zip", done);
     });
   });
 });
