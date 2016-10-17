a0d7f7f55870cf8c49ad8b2ca717fa6cac773ab9
Miquel Colomer <mcolomer@gmail.com>
2016-10-17 09:10:38 +0200

    + fixed ftp auth

--- test/test.js
+++ test/test.js
@@ -27,12 +27,11 @@ describe('anyfile', function () {
   
   it('ftp copy to local', function (done) {
     anyfile.from("ftp://anonymous:miemail%40gmail.com@speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
-      debug(err);
       assert.ok(res);
       deleteFile("100KB.zip", done);
     });
   });
-  
+    
   it('http copy to local', function (done) {
     anyfile.from("http://speedtest.tele2.net/100KB.zip").to("100KB.zip", function(err, res) {
       assert.ok(res);
